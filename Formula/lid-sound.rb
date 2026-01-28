17598ecaa87629371bf835ee808feeab31bdadf6
Manuel Gonzalez <mrgonzalez@applaudostudios.com>
2026-01-28 20:21:12 -0300

    Fix formula (was Swift manifest) and install default sounds

--- Formula/lid-sound.rb
+++ Formula/lid-sound.rb
@@ -9,21 +9,13 @@ class LidSound < Formula
   depends_on "swift" => :build
 
   def install
-    # Avoid SwiftPM trying to use sandbox-exec (can fail under Homebrew's build sandbox)
     ENV["SWIFTPM_DISABLE_SANDBOX"] = "1"
-
-    # Ensure SwiftPM has a writable HOME for its internal state/caches
     ENV["HOME"] = buildpath
-
-    # Align deployment target with the running macOS to avoid linker target mismatches
     ENV["MACOSX_DEPLOYMENT_TARGET"] = MacOS.version.to_s
 
     system "swift", "build", "-c", "release", "--disable-sandbox"
 
-    # Install executable
     bin.install ".build/release/lid-sound"
-
-    # Install default sounds into Homebrew share directory
     (share/"lid-sound/sounds").install Dir["Sources/lid-sound/sounds/*.mp3"]
   end
 
